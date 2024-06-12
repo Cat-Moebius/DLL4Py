ed871e8ea256de3db64261584242b2a9b7c7f5f2
Nizametdinov Bulat <nizametdinov.bulat@mail.ru>
2024-06-12 19:15:38 +0300

    test_dll

---
+++ test_check/test_check/test_check.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = MyLibrary.MyLibrary.Add(5, 3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+
